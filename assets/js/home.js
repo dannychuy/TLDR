0d7b950dd1163025b4931a3a602ba3472bbfca3f
Danny <Danny@Turtles-MacBook-Pro.local>
2017-10-08 03:38:02 -0700

    Drag and Drop

--- assets/js/home.js
+++ assets/js/home.js
@@ -13,24 +13,24 @@ $(document).ready(function() {
 
 
 
-    $("#page-wrapper").click(function(){
-        var $this = $(this);
-        $(document.getElementById("loader-wrapper")).css("visibility", "inherit");
-        $(document.getElementById("button")).css("visibility", "hidden");
-        setTimeout(function(){
-            $('body').addClass('loaded');
+    // $("#page-wrapper").click(function(){
+    //     var $this = $(this);
+    //     $(document.getElementById("loader-wrapper")).css("visibility", "inherit");
+    //     $(document.getElementById("button")).css("visibility", "hidden");
+    //     setTimeout(function(){
+    //         $('body').addClass('loaded');
 
-            $this.css("top", "-120%");
+    //         $this.css("top", "-120%");
 
-            var $card = $(document.getElementById("card"));
-            $card.css("display", "inherit");
-        }, 2000);
+    //         var $card = $(document.getElementById("card"));
+    //         $card.css("display", "inherit");
+    //     }, 2000);
         
-    });
+    // });
 
 
 
-    $("#top-bar").click(up());
+    // $("#top-bar").click(up());
 });
 
 function up(){
