899ccdb02caa1e2636952acdc4ca24a7534ed603
Danny <Danny@Turtles-MacBook-Pro.local>
2017-10-08 01:19:23 -0700

    Front end

---
+++ assets/js/home.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+    var $this = $(document.getElementById("TLDR-title"));
+    setTimeout(function(){ 
+        $this.css("transform", "translateX(0%)");
+        setTimeout(function(){ 
+            $this.children().css("opacity", "1");
+        }, 200);
+    }, 400);
+
+    
+
+    
+
+
+
+    $("#page-wrapper").click(function(){
+        var $this = $(this);
+        $(document.getElementById("loader-wrapper")).css("visibility", "inherit");
+        $(document.getElementById("button")).css("visibility", "hidden");
+        setTimeout(function(){
+            $('body').addClass('loaded');
+
+            $this.css("top", "-120%");
+
+            var $card = $(document.getElementById("card"));
+            $card.css("display", "inherit");
+        }, 2000);
+        
+    });
+
+
+
+    $("#top-bar").click(up());
+});
+
+function up(){
+        var $this = $(document.getElementById("page-wrapper"));
+        console.log("1");
+        $this.css("top", "0%");
+        console.log(3);
+
+        var $card = $(document.getElementById("card"));
+        $card.css("display", "inherit");
+    }
+
